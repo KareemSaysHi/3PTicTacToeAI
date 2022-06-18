6e29218212b816fbb6041ae0b5159e73e3d93a02
CyrilSharma <sharma.cyril@gmail.com>
2022-06-17 22:39:12 -0400

    stuff that doesn't compile

--- tictactoe3.cpp
+++ tictactoe3.cpp
@@ -19,11 +19,12 @@ void tictactoe3::unmove(int tile, int action) {
     return;
 }
 
-vector<Piece> unhash(int n) {
+Piece* unhash(int n) {
     Piece p1 = static_cast<Piece>(n%100);
     Piece p2 = static_cast<Piece>(n%10 - 10*p1);
     Piece p3 = static_cast<Piece>(n - 100*p1 - 10*p2);
-    return {p1, p2, p3};
+    static Piece pieces[3] = {p1, p2, p3};
+    return pieces;
 }
 
 void tictactoe3::display() {
@@ -35,7 +36,7 @@ void tictactoe3::display() {
     std::cout << "" << std::endl;
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
-            vector<Piece> pieces = unhash(state[i][j]);
+            Piece *pieces = unhash(state[i][j]);
             std::cout << ESC + RED << pieces[0] << ESC;
             std::cout << ESC + GREEN << pieces[1] << ESC;
             std::cout << ESC + BLUE << pieces[2] << ESC << " ";
--- tictactoe3.h
+++ tictactoe3.h
@@ -1,24 +1,22 @@
 #ifndef tictactoe3_h
 #define tictactoe3_h
 
-#include <vector>
 #include "pieces.h"
 class tictactoe3 {
 public:
-    int player = 0;
+    int player;
 
     // ongoing, p1-p3 win, tie, 
     int outcome;
 
+    // you can make this a lot better with bitmasking but until efficiency is a priority i'm gonna go with ease of implementation.
     int state[3][3];
 
     tictactoe3();
 
-    tictactoe3(int board[3][3]) { memcpy(board, state, sizeof(board)); }
+    tictactoe3(int board[3][3]) { memcpy(board, state, 9*sizeof(board[0][0])); }
 
     bool is_win(int tile, int action);
-
-    vector<int> is_terminal(int action);
 
     void move(int tile, int action);
 
@@ -28,7 +26,7 @@ public:
 
     int hash(Piece p1, Piece p2, Piece p3) { return p1*100 + p2*10 + p3*1; }
 
-    vector<Piece> unhash(int n);
+    Piece* unhash(int n);
 
     void display();
 };
